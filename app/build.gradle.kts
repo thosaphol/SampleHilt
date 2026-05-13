55a125382b466d00a9fc9e438703b684d256a2fa
thosaphol.vua <thosaphol.vua@bangkoklife.com>
2026-05-13 15:37:45 +0700

    fixed kotlin, jvm, hilt, ksp, agp version compatible

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt.android)
-
 }
 
 android {
     namespace = "com.example.samplehilt"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.samplehilt"
@@ -38,12 +34,20 @@ android {
     }
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
